839aac7f24b2fc149c324be5d936ac822afefbd0
Espen Kørra <eko@difi.no>
2016-12-13 13:37:49 +0100

    removed unused code, environment check, extenced response object

--- server.js
+++ server.js
@@ -8,16 +8,32 @@ var log = require('winston');
 var router = express();
 var server = http.createServer(router);
 
+var baseUri = "https://beta-meldingsutveksling.difi.no";
+
 function getLatestAsync(){
     return new Promise((resolve, reject) =>{
          request.get(options).then(function(body) {  
             var info = JSON.parse(body);
-            var latest = info.data[0].latestSnapshot;
+            var latest = info.data[0].version;
             resolve(latest);
         });
     });
 }
 
+function getEnvrionment(env){
+    switch (env) {
+        case "prod": 
+            return "releases";
+        case "staging":
+            return "staging";
+        case "systest":
+            return "systest"
+        case "itest":
+            return "itest";
+        default:
+            return "none";
+    }
+}
 
 var options = {
     uri: 'https://beta-meldingsutveksling.difi.no/service/local/lucene/search?a=integrasjonspunkt&repositoryId=staging',
@@ -28,46 +44,42 @@ var options = {
     }
 };
 
-function getLatest(){
-    request.get(options).then(function(body) {  
-        var info = JSON.parse(body);
-        var latest = info.data[0].latestSnapshot;
-        console.log(latest);
-        return latest;
-    });
-}
+router.get('/latest', function (req, res) {    
+    var repositoryId;
+    var environment;
+    var latestVersion = new Object();
+    try{
+        environment = req.query.env;
+        repositoryId = getEnvrionment(environment);
+    }
+    catch(err)
+    {
+        log.error("feil !!");
+    }
+    if(repositoryId == "none"){
+        log.error ("unexsisting repositoryId: " +repositoryId);
+        res.statusCode = 400
+        res.statusMessage = environment + " is not a valid environment";
+        res.send();
+        return;
+    }
 
-
-router.get('/latest', function (req, res) {
-    var latest = getLatest();
-    console.log(latest);
-    res.send(latest);
-});
-
-router.get('/latestasync', function (req, res) {
-    env = req.query.env;
-    log.info("env: "+env);
+    latestVersion.repositoryId = repositoryId.toString();   
+    
     Promise.all([getLatestAsync()])
         .then((latest) => {
-            log.info("async-"+latest );
-            res.send(latest);
+            log.info("environment: "+ environment + "version: "+latest );
+            latestVersion.baseVersion = latest.toString();            
+            var latestV = { baseVersion: latest, version: "", sha1: "", repositoryId: environment };
+            var json = JSON.stringify({ 
+                latest: latestVersion
+            });
+            res.send(json)            //res.send(latest);
+           
         })
         .catch((err) => log.error(err));
 });
 
-router.get('/latestartifact', function(req, res){
-    //var latest = await getLatest()
-});
-
- 
-function callback(error, response, body, res) {
-  if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-    var latest = info.data[0].latestSnapshot;
-    console.log(latest);
-    res.send(latest);
-  }
-}
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
