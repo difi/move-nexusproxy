d5f3e00ab928d56f33d26f7912ec1627d1212378
Espen Kørra <ekorra@gmail.com>
2017-06-09 09:58:42 +0200

    added dockerfile, changed port

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ router.get('/latest', function (req, res) {
 
 
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+server.listen(process.env.PORT || 8771, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Server listening at", addr.address + ":" + addr.port);
 });
